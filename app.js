4ba6e823c5687010f7ba6ee7a3b2a596e5fbca28
mokhles95 <mokhleshaj@gmail.com>
2021-09-13 19:13:10 +0100

    env

--- app.js
+++ app.js
@@ -50,5 +50,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json('error');
 });
-app.listen(3000)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+app.listen(process.env.PORT)
 module.exports = app;
--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 const app = require('../app');
 
 // connection to database
-mongoose.connect('mongodb://localhost:27017/e-commerce', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(result => {
+mongoose.connect('mongodb+srv://mokhles:YVXZRH7nKS2yBSUl@cluster0.cwtst.mongodb.net/artisana?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(result => {
   
     console.log("connected to database");
 }).catch(err => {
